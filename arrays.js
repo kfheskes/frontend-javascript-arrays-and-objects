e9bbb99eec3268f732c0e7db329a9c53700049bd
kfheskes <kayarneheskes@hotmail.com>
2023-03-24 14:01:56 +0100

    uitwerkingen

--- arrays.js
+++ arrays.js
@@ -2,17 +2,24 @@
 // 1. Declareer een variabele en sla daar een array met vier jaartallen (zoals 2021) in op. Log dit in de terminal.
 // ==========================================
 
+const years = ['2001', '2002', '2003', '2004'];
+console.log(years);
+
 
 // ==========================================
 // 2. Declareer een variabele en sla daar een array met drie van jouw hobbies (zoals muziek luisteren en koekjes eten) in op. Log dit in de terminal.
 // ==========================================
 
+const hobbies = ['Hardlopen', 'Fietsen', 'Eten'];
+console.log(hobbies);
 
 // ==========================================
 // 3a. Declareer een variabele met daarin een array met de waardes 3, 4, 5, 7 en 2
 // 3b. Zorg er nu voor dat je de eerste waarde uit die array in de terminal logt             // geeft 3
 // ==========================================
 
+const result = ['3','4', '5', '7', '2'];
+console.log(result[0]);
 
 // ==========================================
 // 4a. Declareer een variabele met daarin een array met de waardes groen, geel, rood, paars, blauw en oranje.
@@ -21,12 +28,21 @@
 // 4c. Zorg ervoor dat je de tweede waarde uit bovenstaande array in de terminal logt       // geeft geel
 // ==========================================
 
+const colour = ['groen', 'geel', 'rood', 'paars', 'blauw', 'oranje'];
+console.log(colour[5]);
+console.log(colour[2]);
+
 
 // ==========================================
 // 5a. Declareer een variabele met daarin een array met de waardes 21, 22, 23, 25, 25 en log dit in de terminal
 // 5b. Overschrijf één van de items in de array dusdanig dat de cijferreeks netjes doorloopt (je mag de declaratie op de vorige regel niet aanpassen).
 // 5c. Log de array in de terminal                                                          // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
+
+const temperature = ['21', '22', '23', '25', '25'];
+console.log(temperature);
+temperature[3] = '24';
+console.log(temperature);
 
 
 // ==========================================
@@ -35,3 +51,11 @@
 // 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
+
+const ingredients = ['bladerdeeg', 'knoflook', 'spinazie'];
+ingredients[0] = 'lasagne bladen';
+console.log(ingredients);
+
+const ingredientsLength = ingredients.length;
+
+console.log(ingredientsLength);
